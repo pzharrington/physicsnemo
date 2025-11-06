f07876f407117146a9ace3de68081b45ea029ec2
Peter Harrington <pharrington@nvidia.com>
2025-11-06 00:37:47 +0000

    Revert test and compat map

--- physicsnemo/compat/__init__.py
+++ physicsnemo/compat/__init__.py
@@ -62,8 +62,6 @@ COMPAT_MAP = {
     "physicsnemo.utils.diffusion": "physicsnemo.models.diffusion.utils",
     "physicsnemo.utils.patching": "physicsnemo.nn.patching",
     "physicsnemo.utils.domino": "physicsnemo.models.domino.utils",
-    "physicsnemo.utils.insolation": "physicsnemo.nn.insolation",
-    "physicsnemo.utils.zenith_angle": "physicsnemo.nn.zenith_angle",
     "physicsnemo.launch.utils.checkpoint": "physicsnemo.utils.checkpoint",
     "physicsnemo.launch.logging": "physicsnemo.utils.logging",
 }
