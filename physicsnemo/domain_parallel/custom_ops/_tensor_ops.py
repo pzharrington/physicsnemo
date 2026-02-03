ad9a47a1cdc20f9a0c2aed5ff7861a5f9693dac1
Peter Harrington <pharrington@nvidia.com>
2026-02-02 16:44:51 -0800

    safer torch import

--- physicsnemo/domain_parallel/custom_ops/_tensor_ops.py
+++ physicsnemo/domain_parallel/custom_ops/_tensor_ops.py
@@ -28,25 +28,25 @@ from torch.distributed.tensor._op_schema import (
     OutputSharding,
     RuntimeSchemaInfo,
 )
-
-try:
-    from torch.distributed.tensor._ops.utils import (
-        register_prop_rule,
-    )
-except ImportError:
-    # PyTorch 2.10.0 temporarily moved this
-    from torch.distributed.tensor._ops.registration import (
-        register_prop_rule,
-    )
 from torch.distributed.tensor.placement_types import (
     Partial,
     Replicate,
     Shard,
 )
 
+from physicsnemo.core.version_check import check_version_spec
 from physicsnemo.domain_parallel._shard_tensor_spec import (
     _stride_from_contiguous_shape_C_style,
 )
+
+if check_version_spec("torch", "2.10.0"):
+    from torch.distributed.tensor._ops.registration import (
+        register_prop_rule,
+    )
+else:
+    from torch.distributed.tensor._ops.utils import (
+        register_prop_rule,
+    )
 
 aten = torch.ops.aten
 
